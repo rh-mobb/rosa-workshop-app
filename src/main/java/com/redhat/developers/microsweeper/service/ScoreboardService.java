eff946ca6a67f93afe6a80195b7ab6f5a7be1021
Michael McNeill <michael@michaelryanmcneill.com>
2023-02-06 22:42:37 +0200

    Adding STS support into app

--- src/main/java/com/redhat/developers/microsweeper/service/ScoreboardService.java
+++ src/main/java/com/redhat/developers/microsweeper/service/ScoreboardService.java
@@ -5,6 +5,9 @@ import com.redhat.developers.microsweeper.model.Score;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
+import software.amazon.awssdk.core.SdkSystemSetting;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -15,6 +18,16 @@ public class ScoreboardService extends AbstractService {
 
     @Inject
     DynamoDbClient dynamoDB;
+
+    public ScoreboardService () {
+
+    	System.setProperty(SdkSystemSetting.SYNC_HTTP_SERVICE_IMPL.property(), "software.amazon.awssdk.http.apache.ApacheSdkHttpService");
+
+    	dynamoDB = DynamoDbClient.builder()
+        	.credentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
+		.httpClient(ApacheHttpClient.create())
+        	.build();
+    }
 
     public List<Score> getScoreboard() {
         return dynamoDB.scanPaginator(scanRequest()).items().stream()
